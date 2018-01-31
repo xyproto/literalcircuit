4261d0469ac36520521a1cd3885fbaf63ff94d58
Alexander F Rødseth <rodseth@gmail.com>
2018-01-31 20:45:13 +0100

    BAH!

--- bitchan.go
+++ bitchan.go
@@ -6,24 +6,29 @@ import (
 	"log"
 )
 
-// BitChan is a channel that may return a bit
-type BitChan chan bits.Bit
+type (
+	// BitChan is a channel that may return a bit
+	BitChan chan bits.Bit
 
-// BitChans is a slice of several channels that each may return a bit
-type BitChans [](chan bits.Bit)
+	// BitChans is a slice of several channels that each may return a bit
+	BitChans [](chan bits.Bit)
 
-// StopChan is a boolean channel for making signal propagation stop
-type StopChan chan bool
+	// StopChan is a boolean channel for making signal propagation stop
+	StopChan chan bool
 
-// ChanGate is a gate that deals with channels of bits instead of bits
-type ChanGate func(input BitChans, output BitChan, stop StopChan)
+	// TickChan is for sending clock ticks, true for high, false for low
+	TickChan chan bool
+
+	// ChanGate is a gate that deals with channels of bits instead of bits
+	ChanGate func(input BitChans, output BitChan, stop StopChan, tick TickChan)
+)
 
 // WrapOneToManyGate is a wrapper function that takes a OneToManyGate and
 // returns a ChanGate instead. It deals with channels of bits instead of bits,
 // and waits for bits to be received before using the given OneToManyGate to
 // process the inputs and return an output.
 func WrapOneToManyGate(gateName string, gate bits.OneToManyGate) ChanGate {
-	return func(inputChans BitChans, output BitChan, stop StopChan) {
+	return func(inputChans BitChans, output BitChan, stop StopChan, tick TickChan) {
 		// Continuously gather all needed input bits and output the result as an output bit
 		for {
 			// Stop if a stop signal was received
@@ -40,11 +45,10 @@ func WrapOneToManyGate(gateName string, gate bits.OneToManyGate) ChanGate {
 					inputBits[i] = <-inputChan
 					log.Printf(gateName+": Input %d\t: %v\n", i, inputBits[i])
 				}
-				// Process the input bits and output the result bit
+				// Process the input bits, wait for a clock tick, and output the result bit
 				outputBit := gate(inputBits)
-
+				<-tick
 				log.Printf(gateName+": Output\t\t: %v\n", outputBit)
-
 				output <- outputBit
 			}
 		}
@@ -60,7 +64,7 @@ func WrapTruthTable(gateName string, tt *bits.TruthTable) ChanGate {
 
 // SpewBits continously outputs the given bits (as a string) to the
 // given BitChans, until stopped by a "true" being sent to the stop channel.
-func SpewBits(inputBits *bits.Bits, outputBitChans BitChans, stop StopChan) {
+func SpewBits(inputBits *bits.Bits, outputBitChans BitChans, stop StopChan, tick TickChan) {
 	if len(*inputBits) != len(outputBitChans) {
 		panic(fmt.Sprintf("Wrong number of input bits, should be %d: %v", len(outputBitChans), inputBits))
 	}
@@ -77,6 +81,8 @@ func SpewBits(inputBits *bits.Bits, outputBitChans BitChans, stop StopChan) {
 			return
 		// Must have a default case for the select-case to be non-blocking!
 		default:
+			// Wait for a clock tick
+			<-tick
 			// Output all given bits
 			for i, b := range *inputBits {
 				outputBitChans[i] <- b
@@ -87,7 +93,7 @@ func SpewBits(inputBits *bits.Bits, outputBitChans BitChans, stop StopChan) {
 
 // SpewBitsFromString continously outputs the given bits (as a string) to the
 // given BitChans, until stopped by a "true" being sent to the stop channel.
-func SpewBitsFromString(inputBitString string, outputBitChans BitChans, stop StopChan) {
+func SpewBitsFromString(inputBitString string, outputBitChans BitChans, stop StopChan, tick TickChan) {
 	inputBits, err := bits.StringToBits(inputBitString)
 	if err != nil {
 		panic("Invalid input bit string: " + inputBitString)
@@ -108,6 +114,8 @@ func SpewBitsFromString(inputBitString string, outputBitChans BitChans, stop Sto
 			return
 		// Must have a default case for the select-case to be non-blocking!
 		default:
+			// Wait for a clock tick
+			<-tick
 			// Output all given bits
 			for i, b := range *inputBits {
 				outputBitChans[i] <- b
--- bitchan_test.go
+++ bitchan_test.go
@@ -6,6 +6,12 @@ import (
 	"testing"
 )
 
+func clock(tick TickChan) {
+	for {
+		tick <- true
+	}
+}
+
 func TestWrapAnd(t *testing.T) {
 	i0 := make(BitChan, 1)
 	i1 := make(BitChan, 1)
@@ -14,8 +20,14 @@ func TestWrapAnd(t *testing.T) {
 	// Set up a stopping mechanism
 	stop := make(StopChan, 1)
 
+	// Set up a clock tick
+	tick := make(TickChan, 1)
+
+	// TICK!
+	go clock(tick)
+
 	i := BitChans{i0, i1}
-	go WrapTruthTable("AND", and)(i, o, stop)
+	go WrapTruthTable("AND", and)(i, o, stop, tick)
 
 	i0 <- bits.B1
 	i1 <- bits.B1
@@ -37,11 +49,15 @@ func TestWrapXor(t *testing.T) {
 	o := make(BitChan, 1)
 
 	stop := make(StopChan, 1)
+	tick := make(TickChan, 1)
 
 	i0 <- bits.B0
 	i1 <- bits.B0
 
-	go WrapTruthTable("XOR", xor)(BitChans{i0, i1}, o, stop)
+	// TICK!
+	go clock(tick)
+
+	go WrapTruthTable("XOR", xor)(BitChans{i0, i1}, o, stop, tick)
 
 	// Block until we receive an output bit on o
 	result := <-o
@@ -60,8 +76,12 @@ func TestSpew(t *testing.T) {
 	I1 := make(BitChan, 1)
 
 	stop := make(StopChan, 32)
+	tick := make(TickChan, 32)
 
-	go SpewBitsFromString("1 0", BitChans{I0, I1}, stop)
+	// TICK!
+	go clock(tick)
+
+	go SpewBitsFromString("1 0", BitChans{I0, I1}, stop, tick)
 
 	// Try to consume the outputted bits
 	go func() {
@@ -83,31 +103,37 @@ func TestSpew(t *testing.T) {
 
 func TestWrapCombine(t *testing.T) {
 	// Set up circuit input bits
-	I0 := make(BitChan, 32)
-	I1 := make(BitChan, 32)
+
+	I0 := make(BitChan)
+	I1 := make(BitChan)
 
 	// Stopping mechanism
-	stop := make(StopChan, 32)
+	stop := make(StopChan, 1)
 	stopConsumers := 0 // gate counter, used when stopping all of them
+
+	tick := make(TickChan, 1)
+
+	// TICK!
+	go clock(tick)
 
 	// ----------
 
 	// Set up input/output bits and run the xor gate as a goroutine
 	xorI0 := I0
 	xorI1 := I1
-	xorO0 := make(BitChan, 32) // size
-	go WrapTruthTable("XOR", xor)(BitChans{xorI0, xorI1}, xorO0, stop)
+	xorO0 := make(BitChan) // size
+	go WrapTruthTable("XOR", xor)(BitChans{xorI0, xorI1}, xorO0, stop, tick)
 	stopConsumers++
 
 	// Set up input/output bits and run the xor gate as a goroutine
 	andI0 := xorO0
 	andI1 := I0                // Duplicate input bit 0 as and input bit 1 (will be fed B1 in a loop)
-	andO0 := make(BitChan, 32) // size
-	go WrapTruthTable("AND", and)(BitChans{andI0, andI1}, andO0, stop)
+	andO0 := make(BitChan) // size
+	go WrapTruthTable("AND", and)(BitChans{andI0, andI1}, andO0, stop, tick)
 	stopConsumers++
 
 	// Input the input bits into the circuit, until stopped
-	go SpewBitsFromString("1 0", BitChans{I0, I1}, stop)
+	go SpewBitsFromString("1 0", BitChans{I0, I1}, stop, tick)
 	stopConsumers++
 
 	// Set up the circuit output bit
